2ae4bb06ca2d82e2aef1bfba37524df1edef0328
Caden OP <92657822+Krishnacodr@users.noreply.github.com>
2021-11-11 09:15:15 +0530

    Update Rishabh.py

--- driver/Rishabh.py
+++ driver/Rishabh.py
@@ -3,7 +3,8 @@ import rishabh.py
      ##this bot maket by rishabh xd##
 **Lucy-video-streamer create by
  rishabh xd full credits**
-
+##full credit by https://github.com/mafiarishabh
+##
 
 Print:(rishabh xd)
 
